5c5f09653103ad3958f6107d38f348d390926326
tdnargentina <tdnargentina87@gmail.com>
2025-07-13 12:47:51 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,13 +1,29 @@
 <?php
 
+// Получае данные сделки
+$dealdata = $_POST ["data"]["FIELDS"]["ID"];
+
+//Добавляем полученные данные в файл
 $file = "test.txt";
-$content = "тестовый лог";
+file_put_contents ($file,$dealdata);
 
+// Создаем переменные для передачи значений
+$title = "Выяснить причину удаления сделки";
+$description = "Была удалена сделка ID: $dealdata пожалуйста посмотрите кто удалил и выясните причину";
+$resoinsible = 6;
+$deadline = "2025/07/13";
 
-file_put_contents ($file, $content);
+// Задаем юрл для запуска задачи
+$url = "https://laempresa.bitrix24.es/rest/1/luufo23s7kq9k433/task.item.add.json?"
+. "fields[TITLE]=" . urlencode($title)
+. "&fields[DESCRIPTION]=" . urlencode ($description)
+. "&fields[RESPONSIBLE_ID]=" . urlencode ($resoinsible)
+. "&fields[DEADLINE]=" . urlencode($deadline);
 
-$vivod = file_get_contents($file);
+// Запускаем ЮРЛ
+file_get_contents ($url);
 
-echo $vivod;
+}
+
 
 ?>

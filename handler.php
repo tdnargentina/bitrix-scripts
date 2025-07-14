4f40349cc8254d5c7a48f90999afc57bbf031466
tdnargentina <tdnargentina87@gmail.com>
2025-07-14 14:55:27 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,3 +1,4 @@
+
 <?php
 
 // Получае данные сделки
@@ -5,9 +6,9 @@ $dealdata = $_POST ["data"]["FIELDS"]["ID"];
 
 // Создаем переменные для передачи значений
 $title = "Выяснить причину удаления сделки $dealdata";
-$description = "Была удалена сделка ID: $dealdata пожалуйста, перейдите в корзину и посмотрите кто удалил и выясните причину удаления (задача созданна данным вебхуком https://enterprisesubscription.bitrix24.com/devops/edit/out-hook/19/)";
+$description = "Была удалена сделка ID: $dealdata пожалуйста, перейдите в корзину и посмотрите кто удалил и выясните причину удаления (задача создана данным вебхуком https://enterprisesubscription.bitrix24.com/devops/edit/out-hook/19/)";
 $resoinsible = 17;
-$deadline = "2025/07/30";
+$deadline = "2025/08/25";
 
 // Задаем юрл для запуска задачи
 $url = "https://enterprisesubscription.bitrix24.com/rest/17/n5yegmmn5ob395jp/task.item.add.json?"

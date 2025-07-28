b5417e89182a8c815cf241f406aba87c622437a2
tdnargentina <tdnargentina87@gmail.com>
2025-07-28 18:30:03 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,14 +1,12 @@
 <?php
-
 // Получае данные сделки
 $dealdata = $_POST ["data"]["FIELDS"]["ID"];
-
 
 // получаем данные сделки с аккаунта 1
 
 $urlDealGet = "https://laempresa.bitrix24.es/rest/1/a9wyp5ll5h14nuzm/crm.deal.get.json?ID=$dealdata";
 $dealdata = file_get_contents ($urlDealGet);
-$dealJS = json_decode($dealdata. true);
+$dealJS = json_decode($dealdata, true);
 
 $title = $dealJS["result"]["TITLE"];
 $listaABC = $dealJS ["result"]["UF_CRM_1684250117415"];
@@ -29,6 +27,6 @@ switch ($listaABC){
 
 $urlDealAdd = "https://enterprisesubscription.bitrix24.com/rest/17/n5yegmmn5ob395jp/crm.deal.add.json?"
 ."fields[TITLE]=". urlencode($title)
-."&fields[UF_CRM_1750495914815]=" . urlencode($lestaABC);
+."&fields[UF_CRM_1750495914815]=" . urlencode($listaABC);
 file_get_contents ($urlDealAdd);
 ?>

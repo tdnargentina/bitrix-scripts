601c638a206acb024e7ab458cfbec04d2c3c883d
tdnargentina <tdnargentina87@gmail.com>
2025-08-01 12:31:06 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,29 +1,3 @@
 <?php
-$url = "https://laempresa.bitrix24.es/rest/1/msxclcgydt7lj59k/event.bind.json";
 
-$event = "CATALOG.PRODUCT.ON.ADD";
-$handler = "https://bitrix-scripts.onrender.com"; // Твой хендлер на Render
-
-// Массив данных, который передадим в POST-запросе
-$postData = [
-    'event' => $event,
-    'handler' => $handler
-];
-
-// Инициализация cURL
-$ch = curl_init($url);
-
-// Устанавливаем параметры запроса
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true); // POST-запрос
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData)); // Кодируем массив как form-urlencoded
-
-// Выполняем запрос
-$response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-// Выводим результат
-echo "HTTP Status: $httpCode\n";
-echo "Response:\n$response";
 ?>

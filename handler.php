4341801e4ae12cf9ca3e997b45cced202335375a
tdnargentina <tdnargentina87@gmail.com>
2025-08-01 11:49:03 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,7 +1,7 @@
 <?php
 $title = "проверка вебхука";
 
-$url = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.add.json"
+$url = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.add.json?"
 ."fields[TITLE]=" . urlencode($title);
 
 echo "скрипт работает";

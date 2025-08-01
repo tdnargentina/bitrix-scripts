46bd0078c55d33b53066a4399991d5c4037e56c4
tdnargentina <tdnargentina87@gmail.com>
2025-08-01 12:30:31 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,13 +1,29 @@
 <?php
-$title = "проверка вебхука";
+$url = "https://laempresa.bitrix24.es/rest/1/msxclcgydt7lj59k/event.bind.json";
 
-$url = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.add.json?"
-."fields[TITLE]=" . urlencode($title);
+$event = "CATALOG.PRODUCT.ON.ADD";
+$handler = "https://bitrix-scripts.onrender.com"; // Твой хендлер на Render
 
+// Массив данных, который передадим в POST-запросе
+$postData = [
+    'event' => $event,
+    'handler' => $handler
+];
 
-$leadData= file_get_contents ($url);
-$JSdata = json_decode ($leadData);
-$test = print_r ($JSdata);
+// Инициализация cURL
+$ch = curl_init($url);
 
-echo $test;
+// Устанавливаем параметры запроса
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true); // POST-запрос
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData)); // Кодируем массив как form-urlencoded
+
+// Выполняем запрос
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+// Выводим результат
+echo "HTTP Status: $httpCode\n";
+echo "Response:\n$response";
 ?>

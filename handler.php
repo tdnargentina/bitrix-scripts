895356164283aebea42758b4bc2e0cce9fc59f4f
tdnargentina <tdnargentina87@gmail.com>
2025-08-01 11:59:02 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -4,5 +4,10 @@ $title = "проверка вебхука";
 $url = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.add.json?"
 ."fields[TITLE]=" . urlencode($title);
 
-echo "скрипт работает";
+
+$leadData= file_get_contents ($url);
+$JSdata = json_decode ($leadData);
+$test = print_r ($JSdata);
+
+echo $test;
 ?>

8c074361de1f45db4cedaa012e6e3f6f88433779
tdnargentina <tdnargentina87@gmail.com>
2025-08-04 10:51:30 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,3 +1,10 @@
 <?php
+$title = "создание сделки говорит о том что обработчик работает";
+
+$url = "https://b24-2txbup.bitrix24.com/rest/1/vx7xsiaqsq61bumx/crm.deal.add?"
+. "fields[TITLE]=" . urlencode ($title);
+
+file_get_contents ($url);
 
 ?>
+

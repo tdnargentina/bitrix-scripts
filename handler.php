db951b87a68c473d6eeb0553e1cbb1702f201ffb
tdnargentina <tdnargentina87@gmail.com>
2025-08-09 11:27:39 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,6 +3,9 @@ $leadID = $_GET['lead_id'] ?? null;
 
 $url = "https://enterprisesubscription.bitrix24.com/rest/17/ej33x1qpsr6kxpry/crm.lead.add.json?"
 . "fields[TITLE]=". urlencode($leadID);
+
+file_get_contents ($url);
 ?>
+
 
 

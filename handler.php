a967dfb50723ca5be7cdddccd2f07e8695edde27
tdnargentina <tdnargentina87@gmail.com>
2025-08-10 11:15:40 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,11 +1,22 @@
 <?php
 $leadID = $_GET['lead_id'] ?? null;
 
-$url = "https://enterprisesubscription.bitrix24.com/rest/17/ej33x1qpsr6kxpry/crm.lead.add.json?"
-. "fields[TITLE]=". urlencode($leadID);
+
+$leadGet = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/"
+. "fields[ID]=" . urlencode($leadID);
+
+$leadgetRun = file_get_contents ($leadGet);
+$jsDataLead = json_decode ($leadgetRun,true);
+$country = $jsDataLead["result"]["UF_CRM_1687251842328"]; // получили значение страны
+
+
+
+$urlleadCreate = "https://enterprisesubscription.bitrix24.com/rest/17/ej33x1qpsr6kxpry/crm.lead.add.json?"
+. "fields[TITLE]=". urlencode($country);
 
 file_get_contents ($url);
 ?>
+
 
 
 

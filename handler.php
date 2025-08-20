173ea8d2504e2069c37f7ca374d7ba5976bcb7a1
tdnargentina <tdnargentina87@gmail.com>
2025-08-20 12:21:20 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,6 +1,13 @@
 
+<?php
 
+$title = "testHook";
+$leadGet = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.add.json?"
+. "fields[TITLE]=" . urlencode ($title);
 
+file_get_contents ($leadGet);
+
+?>
 
 
 

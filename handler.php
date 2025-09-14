31be52910fef3a56f3d65f0de38305a33abe4d7c
tdnargentina <tdnargentina87@gmail.com>
2025-09-14 12:06:51 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,9 +3,7 @@
 // В случае выигрыша код передает название Лида и Списочную страну с одного аккаунта на другой
 // В финальной стадии лидов первого аккаунта необходимо указать обработчик и лид https://bitrix-scripts.onrender.com?lead_id={{ID}}
 
-
 $leadID = $_GET['lead_id'] ?? null;
-
 
 $leadGet = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.get.json?"
 . "id=" . urlencode($leadID);
@@ -37,11 +35,10 @@ switch($country){
 $urlleadCreate = "https://enterprisesubscription.bitrix24.com/rest/17/ej33x1qpsr6kxpry/crm.lead.add.json?"
 . "fields[TITLE]=". urlencode($title) // передали название
 . "&fields[UF_CRM_1754813532364]=" . urlencode($country) // передали id значение страны которое отобразится как старна в поле
-. ."&fields[EMAIL][0][VALUE]=" .urlencode ($mail);
+. "&fields[EMAIL][0][VALUE]=" .urlencode ($mail);
 
 file_get_contents ($urlleadCreate);
 ?>
-
 
 
 

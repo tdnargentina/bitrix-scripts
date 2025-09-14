475e83462a4c97ccb99bfd82f031b319258598d1
tdnargentina <tdnargentina87@gmail.com>
2025-09-14 11:50:25 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -2,26 +2,26 @@
 <?php
 
 // получааем данные id того лида который хотим передать
-$laEmpresaLeadID =$_GET ["lead_id"] ?? null;
+$laEmpresaLeadID =//$_GET ["lead_id"] ?? null;
 // создаем хапрос на получания данных по id лида
 $urlLeadGet = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.get.json?"
-."id=". urlencode ($laEmpresaLeadID);
+."id=". urlencode($laEmpresaLeadID);
 // получаем результат запроса
 $getResult = file_get_contents($urlLeadGet);
 //декодируем результат и получаем нужные данные в полях
-$decodResult = json_decode ($getResult);
-$title = $decodeResult ["result"]["TITLE"];
-$mail = $decodeResult ["result"] ["EMAIL"]["TYPE_ID"];
+$decodeResult = json_decode ($getResult, true);
+$title = $decodeResult ["result"]["TITLE"]; 
+$mail = $decodeResult ["result"]["EMAIL"][0]["VALUE"];
 
 // создаем лид на другом аккаунте
 
 $entLeadAdd = "https://enterprisesubscription.bitrix24.com/rest/17/ej33x1qpsr6kxpry/crm.lead.add.json?"
 ."fields[TITLE]=" .urlencode ($title)
-."&fields[EMAIL]=" .urlencode ($mail);
+."&fields[EMAIL][0][VALUE]=" .urlencode ($mail);
 
 $result = file_get_contents($entLeadAdd);
-
 ?>
+
 
 
 

ee4e250ef08bef46e8af997868341e8974428bf6
tdnargentina <tdnargentina87@gmail.com>
2025-09-14 11:59:58 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,25 +1,45 @@
-
 <?php
 
-// получааем данные id того лида который хотим передать
-$laEmpresaLeadID =$_GET ["lead_id"] ?? null;
-// создаем хапрос на получания данных по id лида
-$urlLeadGet = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.get.json?"
-."id=". urlencode($laEmpresaLeadID);
-// получаем результат запроса
-$getResult = file_get_contents($urlLeadGet);
-//декодируем результат и получаем нужные данные в полях
-$decodeResult = json_decode ($getResult, true);
-$title = $decodeResult ["result"]["TITLE"]; 
-$mail = $decodeResult ["result"]["EMAIL"][0]["VALUE"];
+// В случае выигрыша код передает название Лида и Списочную страну с одного аккаунта на другой
+// В финальной стадии лидов первого аккаунта необходимо указать обработчик и лид https://bitrix-scripts.onrender.com?lead_id={{ID}}
 
-// создаем лид на другом аккаунте
 
-$entLeadAdd = "https://enterprisesubscription.bitrix24.com/rest/17/ej33x1qpsr6kxpry/crm.lead.add.json?"
-."fields[TITLE]=" .urlencode ($title)
-."&fields[EMAIL][0][VALUE]=" .urlencode ($mail);
+$leadID = $_GET['lead_id'] ?? null;
 
-$result = file_get_contents($entLeadAdd);
+
+$leadGet = "https://laempresa.bitrix24.es/rest/1/l0fvjh738yy1v0qk/crm.lead.get.json?"
+. "id=" . urlencode($leadID);
+
+$leadgetRun = file_get_contents ($leadGet);
+$jsDataLead = json_decode ($leadgetRun,true);
+$country = $jsDataLead["result"]["UF_CRM_1687251842328"]; // получили значение страны
+$title = $jsDataLead["result"]["TITLE"]; // получили название лида
+$mail = $decodeResult ["result"]["EMAIL"][0]["VALUE"]; // получаем рабочую почту
+
+// в switch меняем id значение полей чтобы в новомм аккаунте появилось нужное значение 
+switch($country){
+	case 1721:
+	$country = 57;
+	break;
+	case 1723:
+	$country =  59;
+	break;
+	case 1725:
+	$country =  61;
+	break;
+	case 1727:
+	$country =  63;
+	break;
+}
+
+
+
+$urlleadCreate = "https://enterprisesubscription.bitrix24.com/rest/17/ej33x1qpsr6kxpry/crm.lead.add.json?"
+. "fields[TITLE]=". urlencode($title) // передали название
+. "&fields[UF_CRM_1754813532364]=" . urlencode($country) // передали id значение страны которое отобразится как старна в поле
+. ."&fields[EMAIL][0][VALUE]=" .urlencode ($mail);
+
+file_get_contents ($urlleadCreate);
 ?>
 
 

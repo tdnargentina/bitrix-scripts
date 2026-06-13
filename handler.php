802f87cc7b372f070cdd51f7edda17fb11245fc7
tdnargentina <tdnargentina87@gmail.com>
2026-06-13 12:14:29 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,1 +1,29 @@
 
+<?php
+$domain  = 'aaavito.bitrix24.ru';
+$webhook = '1/nthi0g5a4kz8biro';
+
+$lead_ID = $_GET['lead_id'] ?? null;
+
+$urlGET = "https://{$domain}/rest/{$webhook}/crm.lead.update.json";
+
+$fields = [
+'id' => $lead_ID,
+"fields" => [
+'TITLE' => 'Проверка связи'
+]
+];
+
+
+$params = [
+'http'=> [
+'method' => 'POST',
+'header' => 'Content-Type: application/x-www-form-urlencoded',
+'content' => http_build_query ($fields)
+]
+];
+
+$upakovka = stream_context_create($params);
+
+$zapusk = file_get_contents ($urlGET, false,$upakovka);
+?>

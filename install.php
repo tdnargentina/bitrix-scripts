742092a916ac3e610a3c2f9f89df6448a09f23de
tdnargentina <tdnargentina87@gmail.com>
2026-06-29 11:57:15 +0300

    Update install.php

--- install.php
+++ install.php
@@ -1,7 +1,17 @@
 <?php
 
-header('Content-Type: text/plain; charset=utf-8');
+$data = [
+    'DOMAIN'      => $_REQUEST['DOMAIN'] ?? '',
+    'AUTH_ID'     => $_REQUEST['AUTH_ID'] ?? '',
+    'REFRESH_ID'  => $_REQUEST['REFRESH_ID'] ?? '',
+    'SERVER_ENDPOINT' => $_REQUEST['SERVER_ENDPOINT'] ?? '',
+    'AUTH_EXPIRES' => time() + ($_REQUEST['AUTH_EXPIRES'] ?? 3600)
+];
 
-echo "REQUEST:\n\n";
-print_r($_REQUEST);
+file_put_contents(
+    __DIR__.'/auth.json',
+    json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+);
+
+echo "Приложение успешно установлено";
 ?>

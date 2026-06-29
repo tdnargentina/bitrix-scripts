d13eec2bcecee99348a6cba02dae9516a707c7bf
tdnargentina <tdnargentina87@gmail.com>
2026-06-29 12:10:18 +0300

    Update rest.php

--- rest.php
+++ rest.php
@@ -4,23 +4,21 @@ function callRest($method, array $fields = [])
 {
     $auth = json_decode(file_get_contents(__DIR__.'/auth.json'), true);
 
-    $url = "https://{$auth['DOMAIN']}/rest/{$method}.json";
-
-    $fields['auth'] = $auth['AUTH_ID'];
+    $url = "https://{$auth['DOMAIN']}/rest/{$method}.json?auth=".$auth['AUTH_ID'];
 
     $options = [
         'http' => [
             'method' => 'POST',
             'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
-            'content' => http_build_query($fields)
+            'content' => http_build_query($fields),
+            'ignore_errors' => true
         ]
     ];
 
     $context = stream_context_create($options);
 
-    return json_decode(
-        file_get_contents($url, false, $context),
-        true
-    );
+    $result = file_get_contents($url, false, $context);
+
+    return json_decode($result, true);
 }
 ?>
